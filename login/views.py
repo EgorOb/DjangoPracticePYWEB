373117dd4633a7fa6f32ae9f6896cb21e57b97b5
Egor <egorobidin@inbox.ru>
2023-05-28 19:55:02 +0300

    Работают post в login

--- login/views.py
+++ login/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
 from django.views import View
+from django.http import JsonResponse
 
 
 class LoginView(View):
 
     def get(self, request):
         return render(request, "login/index.html")
+
+    def post(self, request):
+        return JsonResponse(request.POST, json_dumps_params={"indent": 4})
