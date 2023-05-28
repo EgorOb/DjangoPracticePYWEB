4ed90f5031d2db2e18a3a81fcf9243e038cffc0e
Egor <egorobidin@inbox.ru>
2023-05-28 18:42:17 +0300

    Решена самостоятельная

--- other/urls.py
+++ other/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from .views import CurrentDateView
+from .views import CurrentDateView, RandomView, HelloView
 
 urlpatterns = [
     path('datetime/', CurrentDateView.as_view()),
+    path('random/', RandomView.as_view()),
+    path('hello/', HelloView.as_view()),
 ]
--- other/views.py
+++ other/views.py
@@ -2,9 +2,21 @@ from datetime import datetime
 
 from django.views import View
 from django.http import HttpResponse
+from random import random
 
 
 class CurrentDateView(View):
     def get(self, request):
         html = f"{datetime.now()}"
         return HttpResponse(html)
+
+
+class RandomView(View):
+    def get(self, request):
+        random_number = random()
+        return HttpResponse(random_number)
+
+
+class HelloView(View):
+    def get(self, request):
+        return HttpResponse("""<h1>Hello, World</h1>""")
