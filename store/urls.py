8c710bf376355ecd0c8cd4a6162e6b7f2e219d72
Egor <egorobidin@inbox.ru>
2023-05-30 15:08:40 +0300

    Настроены переходы между страницами и сделан преход на конкретный продукт.

--- store/urls.py
+++ store/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .views import ShopView, CartView, ProductSingleView
 
+app_name = 'store'
+
 urlpatterns = [
-    path('', ShopView.as_view()),
-    path('cart/', CartView.as_view()),
-    path('product/', ProductSingleView.as_view())
+    path('', ShopView.as_view(), name='shop'),
+    path('cart/', CartView.as_view(), name='cart'),
+    path('product/<int:id>/', ProductSingleView.as_view(), name='product')
 ]
