bb6066a78e18ce398143834f6f74cb50752be315
egoro <egorobidin@inbox.ru>
2023-06-01 20:05:11 +0300

    Добавлены продукты и категории

--- store/models.py
+++ store/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+  name = models.CharField(max_length=255)
+
+  def __str__(self):
+      return self.name
+
+
+class Product(models.Model):
+  name = models.CharField(max_length=255)
+  description = models.TextField()
+  price = models.DecimalField(max_digits=10, decimal_places=2)
+  image = models.ImageField(upload_to='static/products/', null=True, blank=True)
+  category = models.ForeignKey(Category, on_delete=models.CASCADE)
+  created_time = models.DateTimeField(auto_now_add=True)
+  updated_time = models.DateTimeField(auto_now=True)
+
+  def __str__(self):
+      return self.name
