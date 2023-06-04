0af78a0173d3c62097bf95c044cbf02ec1b58e29
Egor <egorobidin@inbox.ru>
2023-06-04 21:24:34 +0300

    Добавлены продукты в БД

---
+++ script.py
@@ -0,0 +1,55 @@
+import django
+import os
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
+django.setup()
+
+from store.models import Product, Category
+
+if __name__ == "__main__":
+    category = Category.objects.get(name='Vegetables')
+    obj = Product(name='Carrots', description='Морковь', price=120,
+                  image='static/product-7.jpg',
+                  category=category)
+    obj.save()
+
+    obj = Product.objects.create(name='Fruit Juice',
+                                 description='Фруктовый сок',
+                                 price=120,
+                                 image='static/product-8.jpg',
+                                 category=Category.objects.get(name='Juice'))
+    data = ({'name': 'Onion',
+             'price': 120.00,
+             'description': "Лук",
+             'image': 'static/product-9.jpg',
+             'category': 'Vegetables'},
+            {'name': 'Apple',
+             'price': 120.00,
+             'description': "Яблоко",
+             'image': 'static/product-10.jpg',
+             'category': 'Fruits'},
+            {'name': 'Garlic',
+             'price': 120.00,
+             'description': "Чеснок",
+             'image': 'static/product-11.jpg',
+             'category': 'Vegetables'},
+            )
+
+    categ = {'Fruits': Category.objects.get(name='Fruits'),
+             'Vegetables': Category.objects.get(name='Vegetables'),
+             }
+
+    objects_to_create = [Product(name=val['name'],
+                                 description=val['description'],
+                                 price=val['price'],
+                                 image=val['image'],
+                                 category=categ[val['category']]) for val in
+                         data]
+
+    Product.objects.bulk_create(objects_to_create)
+
+    Product.objects.create(name='Chilli',
+                           description='Перец Чили',
+                           price=120,
+                           image='static/product-12.jpg',
+                           category=Category.objects.get(name='Vegetables'))
