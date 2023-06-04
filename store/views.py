7e47c97b237cf4628f51a205bff154c479da5329
Egor <egorobidin@inbox.ru>
2023-06-05 00:07:03 +0300

    Вывод товара в shop, но картинки выводятся неправильно

--- store/views.py
+++ store/views.py
@@ -1,77 +1,27 @@
 from django.shortcuts import render
 from django.views import View
+from django.db.models import OuterRef, Subquery, F, ExpressionWrapper, DecimalField
+from .models import Product, Discount
 
 
 class ShopView(View):
 
     def get(self, request):
-        context = {'data': [{'name': 'Bell Pepper',
-                             'discount': 30,
-                             'price_before': 120.00,
-                             'price_after': 80.00,
-                             'id': 1,
-                             'url': 'store/images/product-1.jpg'},
-                            {'name': 'Strawberry',
-                             'discount': None,
-                             'price_before': 120.00,
-                             'id': 2,
-                             'url': 'store/images/product-2.jpg'},
-                            {'name': 'Green Beans',
-                             'discount': None,
-                             'price_before': 120.00,
-                             'id': 3,
-                             'url': 'store/images/product-3.jpg'},
-                            {'name': 'Purple Cabbage',
-                             'discount': None,
-                             'price_before': 120.00,
-                             'id': 4,
-                             'url': 'store/images/product-4.jpg'},
-                            {'name': 'Tomatoe',
-                             'discount': 30,
-                             'price_before': 120.00,
-                             'price_after': 80.00,
-                             'id': 5,
-                             'url': 'store/images/product-5.jpg'},
-                            {'name': 'Brocolli',
-                             'discount': None,
-                             'price_before': 120.00,
-                             'id': 6,
-                             'url': 'store/images/product-6.jpg'},
-                            {'name': 'Carrots',
-                             'discount': None,
-                             'price_before': 120.00,
-                             'id': 7,
-                             'url': 'store/images/product-7.jpg'},
-                            {'name': 'Fruit Juice',
-                             'discount': None,
-                             'price_before': 120.00,
-                             'id': 8,
-                             'url': 'store/images/product-8.jpg'},
-                            {'name': 'Onion',
-                             'discount': 30,
-                             'price_before': 120.00,
-                             'price_after': 80.00,
-                             'id': 9,
-                             'url': 'store/images/product-9.jpg'},
-                            {'name': 'Apple',
-                             'discount': None,
-                             'price_before': 120.00,
-                             'id': 10,
-                             'url': 'store/images/product-10.jpg'},
-                            {'name': 'Garlic',
-                             'discount': None,
-                             'price_before': 120.00,
-                             'id': 11,
-                             'url': 'store/images/product-11.jpg'},
-                            {'name': 'Chilli',
-                             'discount': None,
-                             'price_before': 120.00,
-                             'id': 12,
-                             'url': 'store/images/product-12.jpg'},
-                            ]
-                   }
+        price_with_discount = ExpressionWrapper(
+            F('price') * (100.0 - F('discount_value')) / 100.0,
+            output_field=DecimalField(max_digits=10, decimal_places=2)
+        )
 
-        return render(request, 'store/shop.html', context)
+        products = Product.objects.annotate(
+            discount_value=Subquery(
+                Discount.objects.filter(product_id=OuterRef('id')).values(
+                    'value')
+            ),
+            price_before=F('price'),
+            price_after=price_with_discount
+        ).values('id', 'name', 'image', 'price_before', 'price_after',
+                 'discount_value')
+        return render(request, 'store/shop.html', {"data": products})
 
 
 class CartView(View):
