d87cb61efb28c3a8a4f34b0c873d73945ddd1da9
egoro <egorobidin@inbox.ru>
2023-06-07 19:48:56 +0300

    Добавлена проверка в форме

--- login/forms.py
+++ login/forms.py
@@ -8,6 +8,13 @@ from django.forms import EmailField
 class CustomUserCreationForm(UserCreationForm):
     email = EmailField()
 
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        if User.objects.filter(email=email).exists():
+            raise ValidationError('Email Already Exists')
+
+        return email
+
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
