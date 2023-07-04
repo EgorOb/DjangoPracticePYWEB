6547d0b95332a0f94af7db54d042fb87ec0c76f3
Egor <egorobidin@inbox.ru>
2023-07-04 16:05:47 +0300

    change settings.py

--- project/settings.py
+++ project/settings.py
@@ -134,7 +134,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 # if 'localhost' in ALLOWED_HOSTS:
 #     STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # else:
